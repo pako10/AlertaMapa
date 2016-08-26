f2af502f019130638f1dcb5288eb733e0c22b3c2
pako10 <pakoAndrade@Eduardos-MacBook-Pro.local>
2016-08-25 19:18:51 -0500

    AlertMaps

---
+++ app/src/main/java/com/example/pakoandrade/alertamapa/MainActivity.java
@@ -0,0 +1,196 @@
+package com.example.pakoandrade.alertamapa;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.RequestParams;
+import com.loopj.android.http.TextHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import cz.msebera.android.httpclient.Header;
+
+public class MainActivity extends AppCompatActivity {
+    String idAlert = "";
+    Button btAtender;
+    ListView listAlert;
+    String lat = "";
+    String longs = "";
+
+    JSONArray jsonArray;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        btAtender = (Button) findViewById(R.id.btAtender);
+        listAlert = (ListView) findViewById(R.id.listAlert);
+
+
+
+
+
+        listAlert.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, final int position, long id) {
+                readAlertId(position);
+                for (int n = 0; n < listAlert.getChildCount(); n++) {
+                    if(position == n ){
+                        listAlert.getChildAt(n).setBackgroundColor(getResources().getColor(R.color.colorOnListItemSelected));
+                    }else{
+                        listAlert.getChildAt(n).setBackgroundColor(Color.TRANSPARENT);
+                    }
+                }
+                Intent i = new Intent(MainActivity.this, MapsActivity.class);
+                i.putExtra("latitud",lat);
+                i.putExtra("longitud",longs);
+                Toast.makeText(MainActivity.this, lat, Toast.LENGTH_SHORT).show();
+                startActivity(i);
+
+
+            }
+        });
+
+        btAtender.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeStatus();
+            }
+        });
+
+       // readAlert();
+        callAsynchronousTask();
+
+
+    }
+
+    public void readAlert(){
+
+        final ArrayAdapter<String> adapter =
+                new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item );
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        final ArrayAdapter<String> adaptador = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+
+        listAlert.setAdapter(adaptador);
+
+        AsyncHttpClient client = new AsyncHttpClient();
+
+        client.get("http://wsars.cloudapp.net/wsARS.svc/NoAtendidas", new TextHttpResponseHandler() {
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+
+            }
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, String responseString) {
+                try {
+                    JSONObject jsonObject = new JSONObject(responseString);
+                    jsonArray = (JSONArray) jsonObject.opt("d");
+
+                    for(int i = 0;i < jsonArray.length();i++){
+                        JSONObject jsonObjeto = jsonArray.getJSONObject(i);
+                        String idAlerta = jsonObjeto.optString("Alerta");
+
+
+                        listAlert.setAdapter(adaptador);
+                        adaptador.add(idAlerta);
+                    }
+
+                }catch (Exception e){
+
+                }
+            }
+        });
+    }
+
+    public void readAlertId(final int value) {
+
+
+        try {
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject jsonObjeto = jsonArray.getJSONObject(i);
+
+                if (value == i) {
+                    idAlert = jsonObjeto.optString("idAlerta");
+                    lat = jsonObjeto.optString("Lat");
+                    longs =   jsonObjeto.optString("Lon");
+                   // Toast.makeText(MainActivity.this, idAlert + lat, Toast.LENGTH_SHORT).show();
+                    //Log.d("Numero de alerta ", idAlert);
+                }
+            }
+
+        } catch (Exception e) {
+
+        }
+    }
+
+    public void changeStatus(){
+        AsyncHttpClient client = new AsyncHttpClient();
+        RequestParams params = new RequestParams();
+        params.put("idAlerta",idAlert);
+
+        client.get("http://wsars.cloudapp.net/wsARS.svc/AtenderAlerta", params, new TextHttpResponseHandler() {
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+
+            }
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, String responseString) {
+                Toast.makeText(MainActivity.this, "Alerta cambiada", Toast.LENGTH_SHORT).show();
+                readAlert();
+            }
+        });
+    }
+
+    public void callAsynchronousTask() {
+        final Handler handler = new Handler();
+        Timer timer = new Timer();
+        TimerTask doAsynchronousTask = new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(new Runnable() {
+                    public void run() {
+                        try {
+                           readAlert();
+                        } catch (Exception e) {
+                            // TODO Auto-generated catch block
+                        }
+                    }
+                });
+            }
+        };
+        timer.schedule(doAsynchronousTask, 0, 50000); //execute in every 50000 ms
+    }
+
+}
---
+++ app/src/main/java/com/example/pakoandrade/alertamapa/MapsActivity.java
@@ -0,0 +1,83 @@
+package com.example.pakoandrade.alertamapa;
+
+import android.content.Intent;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    String lat = "";
+    String lon = "";
+
+
+    int latI;
+    int lonI;
+    Double latD,lonD,latD2,lonD2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        Intent i = getIntent();
+        Bundle extra = i.getExtras();
+
+        if(extra != null) {
+            lat = (String) extra.get("latitud");
+            lon = (String) extra.get("longitud");
+            Toast.makeText(this, "Se recibieron los valores", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, lat + lon, Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(this, "No se reciben valores", Toast.LENGTH_LONG).show();
+        }
+
+/*
+        latI = Integer.parseInt(lat);
+        lonI = Integer.parseInt(lon);
+        latD = (Double) Double.parseDouble(String.valueOf(latI));
+        lonD = (Double) Double.parseDouble(String.valueOf(latI));*/
+
+        latD2 = Double.parseDouble(lat);
+        lonD2 = Double.parseDouble(lon);
+        Toast.makeText(this, latD2.toString(), Toast.LENGTH_SHORT).show();
+       // Toast.makeText(this, lat, Toast.LENGTH_SHORT).show();
+        Log.d("Latitud", lat);
+        Log.d("longitud", lon);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+       // LatLng sydney = new LatLng(-34, 151);
+        LatLng sydney = new LatLng(latD2, lonD2);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.moveCamera(CameraUpdateFactory.zoomTo(15));
+    }
+}
